3ab0957bb7919397f832000a6dc0d87267cd76b6
Faisal Rana <inquisitive@gmail.com>
2021-02-13 01:29:35 -0800

    added test 1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+let numArray = []
+let str = 10
+let one = "1"
+let result
+for (var i=1; i<=str; i++) {
+  var numbers =+ i;
+  numArray.push(i);
+}
+let numString = numArray.toString()
+for(var j=0; j<=str; j++) {
+  if (numArray[j] === 1) {
+    let result = "positive"
+  }
+  
+}

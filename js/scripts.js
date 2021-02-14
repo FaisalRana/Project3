67c2c31fef5a74988a6af630f4d22887f1fca5d9
Faisal Rana <inquisitive@gmail.com>
2021-02-13 21:31:37 -0800

    test 4 is working

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
 let numArray = []
 let str = 10
-let one = "1"
-let result = ""
+
 for (var i=1; i<=str; i++) {
   var numbers =+ i;
   numArray.push(i);
 }
+
 let numString = numArray.toString().split(",")
+
 for(var j=0; j<=str; j++) {
   if (numString[j].includes(3) === true) {
     numString[j] = "Won't you be my neighbor?"
   } else if(numString[j].includes(2) === true) {
     numString[j] = "boop"
- 
+  } else if (numString[j].includes(1) === true) { 
+    numString[j] = "beep"
+  } else {
     // do nothing
   }
 }
-numString
-
+let theEnd = numString.join(" ")

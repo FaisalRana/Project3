f2c5a3417c96e33a036e366019d779a52e761784
Faisal Rana <inquisitive@gmail.com>
2021-02-13 18:44:44 -0800

    test 2 is working

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 let numArray = []
 let str = 10
 let one = "1"
-let result
+let result = ""
 for (var i=1; i<=str; i++) {
   var numbers =+ i;
   numArray.push(i);
 }
-let numString = numArray.toString()
+let numString = numArray.toString().split(",")
 for(var j=0; j<=str; j++) {
-  if (numArray[j] === 1) {
-    let result = "positive"
-  }
-  
+  if (numString[j] === "Beep") {
+    numString[j] = ""
+  } 
+numArray
 }

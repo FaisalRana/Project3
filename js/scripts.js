d827bf04bbc5ac70a8c81e7d6ea465ace0f07a0f
Faisal Rana <inquisitive@gmail.com>
2021-02-14 03:02:57 -0800

    added everything to scripts.js.. stopped functioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,30 @@
 
 var str1 = 0
+var numArray = [];
+var numArrayS = [];
 $(document).ready(function() {
   $("#formR").submit(function(event) {
     alert("hello");
     str1 += parseInt($("input#inputI").val());
+    for (var i=1; i<=str1; i++) {
+      numArray.push(i);
+    }
+    numArray = numArray.toString().split(",");
+    numArrayS = numArray
+    for(var j=0; j<=numArrayS.length; j++) {
+      if (numArrayS[j].includes(3) === true) {
+        numArrayS[j] = "Won't you be my neighbor?";
+      } else if(numArrayS[j].includes(2) === true) {
+        numArrayS[j] = "boop";
+      } else if (numArrayS[j].includes(1) === true) { 
+        numArrayS[j] = "beep";
+      } else {
+        numArray = numArray;
+      };
+    };
     event.preventDefault();
+   });
   });
-});
+  
 
-var numArray = [];
-for (var i=1; i<=str1; i++) {
-  numArray.push(i);
-}
-var numString = numArray.toString().split(",");
-for(var j=0; j<=numString.length; j++) {
-  if (numString[j].includes(3) === true) {
-    numString[j] = "Won't you be my neighbor?";
-  } else if(numString[j].includes(2) === true) {
-    numString[j] = "boop";
-  } else if (numString[j].includes(1) === true) { 
-    numString[j] = "beep";
-  } else {
-    var nothing = "nothing";
-  }
-}
+
